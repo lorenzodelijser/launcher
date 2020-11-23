1092b0454194bf7d3a5e418abaf0ffb4bc2c731f
Lorenzo <29688706+lorenzodelijser@users.noreply.github.com>
2020-11-23 23:01:27 +0100

    Translated Dutch to English

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,12 @@ let curHr = today.getHours()
 let user = "Lorenzo"
 
 if (curHr >= 6 && curHr < 12) {
-  document.getElementById("welcome").innerHTML = `Goedemorgen ${user}!`
-  document.getElementById("title").innerHTML = `👋 Goedemorgen ${user}!`
+  document.getElementById("welcome").innerHTML = `Good morning ${user}!`
+  document.getElementById("title").innerHTML = `Good morning ${user}!`
 } else if (curHr >= 12 && curHr < 17 ) {
-  document.getElementById("welcome").innerHTML = `Goedemiddag ${user}!`
-  document.getElementById("title").innerHTML = `👋 Goedemiddag ${user}!`
+  document.getElementById("welcome").innerHTML = `Good afternoon ${user}!`
+  document.getElementById("title").innerHTML = `Good afternoon ${user}!`
 } else {
-  document.getElementById("welcome").innerHTML = `Goedenavond ${user}!`
-  document.getElementById("title").innerHTML = `👋 Goedenavond ${user}!`
+  document.getElementById("welcome").innerHTML = `Good Evening ${user}!`
+  document.getElementById("title").innerHTML = `Good Evening ${user}!`
 }
