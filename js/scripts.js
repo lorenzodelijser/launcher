58b178db685f2957c7a11f84e61c73254266c93c
Lorenzo <29688706+lorenzodelijser@users.noreply.github.com>
2020-11-24 00:56:45 +0100

    Added script for browsers that dont support prefers-color-scheme

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,13 @@ if (curHr >= 6 && curHr < 12) {
 
 document.getElementById('welcome').innerHTML = message
 document.getElementById('title').innerHTML = message
+
+// If `prefers-color-scheme` is not supported, fall back to light mode.
+// In this case, light.css will be downloaded with `highest` priority.
+if (window.matchMedia('(prefers-color-scheme: dark)').media === 'not all') {
+  document.documentElement.style.display = 'none';
+  document.head.insertAdjacentHTML(
+      'beforeend',
+      '<link rel="stylesheet" href="./css/light.css" onload="document.documentElement.style.display = \'\'">'
+  );
+}
